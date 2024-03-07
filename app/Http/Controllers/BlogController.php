9f9012d79e25f8d0dca0d8eae1eb3838e6bddcfc
muhammed <muhammedoderinu@gmail.com>
2024-03-07 19:12:19 +0100

    paginate error debugged

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -12,10 +12,10 @@ class BlogController extends Controller
 {
     public function index(){
         $new_posts = Post::whereDateIsAfter('created_at', Carbon::now()->subDays(7)->endOfWeek())
-        ->paginate( $perPage = 2, $columns = ['*'], $pageName = 'blog')->load(['media', 'category']);
+        ->paginate(5)->load(['media', 'category']);
 
         $old_posts = Post::whereDateIsBefore('created_at', Carbon::now()->subDays(7)->endOfWeek())
-        -> paginate( $perPage = 2, $columns = ['*'], $pageName = 'blog')->load(['media', 'category']);
+        -> paginate(5)->load(['media', 'category']);
 
         $first_post = Post::orderBy('created_at', 'desc')->paginate(1)->load(['media', 'category']);
 
